5001c162023c66954c1d494be40851b76953ffa2
yeopto <gunyeop530@gmail.com>
2025-01-02 23:55:46 +0900

    feat: 메서드 정의 및 애너테이션, route 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,55 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    // return this.appService.getHello();
-    return 'hello yeopto';
+  @Get('movie')
+  getMovies() {
+    return [
+      {
+        id: 1,
+        name: '해리포터',
+        character: ['해리포터', '볼드모트'],
+      },
+      {
+        id: 2,
+        name: '반지의 제왕',
+        character: ['간달프'],
+      },
+    ];
+  }
+
+  @Get('movie/:id')
+  getMovie() {
+    return {
+      id: 1,
+      name: '해리포터',
+      character: ['해리포터', '볼드모트'],
+    };
+  }
+
+  @Post('movie')
+  postMovie() {
+    return {
+      id: 3,
+      name: '어벤져스',
+      character: ['아이언맨', '캡틴아메리카'],
+    };
+  }
+
+  @Patch('movie/:id')
+  patchMovie() {
+    return {
+      id: 3,
+      name: '어벤져스',
+      character: ['아이언맨', '블랙위도우'],
+    };
+  }
+
+  @Delete('movie')
+  deleteMovie() {
+    return 3;
   }
 }

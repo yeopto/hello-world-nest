da419edaeb6170f261a90516207b13ecbe680799
yeopto <gunyeop530@gmail.com>
2025-01-07 00:25:55 +0900

    feat: movie 필드 선언 및 get요청 데이터 일관성 있게 변경, Exception 사용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,33 +1,48 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+
+interface Movie {
+  id: number;
+  title: string;
+}
 
 @Controller('movie')
 export class AppController {
+  private movies: Movie[] = [
+    {
+      id: 1,
+      title: '해리포터',
+    },
+    {
+      id: 2,
+      title: '반지의 제왕',
+    },
+  ];
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getMovies() {
-    return [
-      {
-        id: 1,
-        name: '해리포터',
-        character: ['해리포터', '볼드모트'],
-      },
-      {
-        id: 2,
-        name: '반지의 제왕',
-        character: ['간달프'],
-      },
-    ];
+    return this.movies;
   }
 
   @Get(':id')
-  getMovie() {
-    return {
-      id: 1,
-      name: '해리포터',
-      character: ['해리포터', '볼드모트'],
-    };
+  getMovie(@Param('id') id: string) {
+    const movie = this.movies.find((m) => m.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('존재하지 않는 ID의 값의 영화입니다.');
+    }
+
+    return movie;
   }
 
   @Post()

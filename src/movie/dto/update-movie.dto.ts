1026cfae1f0267f4149842fefea6a662d7955d47
yeopto <gunyeop530@gmail.com>
2025-08-26 18:46:24 +0900

    feat: CustomValidator 및 validatorpipe whiteList 옵션

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,4 +1,33 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint()
+class PasswordValidator implements ValidatorConstraintInterface {
+  validate(value: any): Promise<boolean> | boolean {
+    // 비밀번호 길이는 4~8
+    return value.length >= 4 && value.length <= 8;
+  }
+  defaultMessage?(): string {
+    return '비밀번호 길이는 4~8자 이어야 합니다.';
+  }
+}
+
+function IsPasswordValidate(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      validator: PasswordValidator,
+    });
+  };
+}
 
 export class UpdateMovieDto {
   @IsNotEmpty()
@@ -8,4 +37,7 @@ export class UpdateMovieDto {
   @IsNotEmpty()
   @IsOptional()
   genre?: string;
+
+  @IsPasswordValidate()
+  test: string;
 }
